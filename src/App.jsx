37ccaa034e998ddd1000c00b78720656995fa5c3
Bak Wong <thesoulrain@gmail.com>
2025-11-10 22:10:30 -0800

    Update working files

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    <div style={{ padding: "40px", fontSize: "24px", textAlign: "center" }}>
+      <h1>Welcome to Soulrain Studio</h1>
+      <p>Your site is working!</p>
+    </div>
+  );
+}
